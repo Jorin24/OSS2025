84fbb199e6887e92220c87a52bf8cce61fde0ba8
Jorin24 <toonlink0120@gmail.com>
2025-03-11 03:32:27 +0900

    Title: 변수 값 복사하기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250311/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    a=1;
+    b=5;
+    c=3;
+
+    c=a;
+    a=a+c;
+    b=b-c;
+
+    printf("%d\n%d\n%d",a,b,c);
+    
+    return 0;
+}
