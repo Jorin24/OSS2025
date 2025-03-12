f958f2afa6dbcbf79c05fddb8e8b1e27849a3a1d
Jorin24 <toonlink0120@gmail.com>
2025-03-12 11:40:23 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250312/점수 비교/score-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A_math, A_eng, B_math, B_eng;
+    cin>>A_math>>A_eng;
+    cin>>B_math>>B_eng;
+    if(A_math>B_math && A_eng>B_eng) cout << 1;
+    else cout << 0;
+    return 0;
+}
