abd4a27dca000ce0499d22f8ebe43ea5b9d421ff
Jorin24 <toonlink0120@gmail.com>
2025-03-12 11:46:56 +0900

    Title: 남녀노소 구분짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250312/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Gender, age;
+    cin>>Gender;
+    cin>>age;
+
+    if(gender==0)
+    {
+        if(age>=19) cout<<"MAN";
+        else cout<<"BOY";
+
+    }
+    else
+    {
+        if(age>=) cout<<"WOMAN";
+        else cout<<"GIRL";
+    }
+    return 0;
+}
