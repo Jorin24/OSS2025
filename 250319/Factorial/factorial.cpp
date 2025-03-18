bbaf228279b94a535d6c15e755d638298f06199d
Jorin24 <toonlink0120@gmail.com>
2025-03-19 01:07:47 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250319/Factorial/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int factorial(int N) {
+    if(N==1)
+    {
+        return 1;
+    }
+    return (N) * factorial(N-1);
+}
+
+int main() {
+    cin >> N;
+    cout << factorial(N);
+
+    return 0;
+}
