1c61fbf96344683a253364ec68864c597919eed0
Jorin24 <toonlink0120@gmail.com>
2025-04-12 01:27:19 +0900

    Title: 정수 명령 처리 5, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250412/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+string command[10000];
+int num[10000];
+vector<int> arr;
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push_back" || command[i] == "get") {
+            cin >> num[i];
+            if(command[i] == "push_back") {
+                arr.push_back(num[i]);
+            }
+            else {
+                cout << arr[num[i]-1] << endl;
+            }
+        }
+        else if(command[i]=="pop_back") arr.pop_back();
+        else if(command[i]=="size") cout << arr.size() << endl;
+    }
+
+    
+    // Please write your code here.
+
+    return 0;
+}
