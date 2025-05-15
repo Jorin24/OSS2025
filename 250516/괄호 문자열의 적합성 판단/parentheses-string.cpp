99c120cbbdae72a3dc98f734917d1e841447a6db
Jorin24 <toonlink0120@gmail.com>
2025-05-16 02:53:52 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250516/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 250516/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -5,21 +5,30 @@
 using namespace std;
 
 string str;
-string data[100];
 
 int main() {
-    stack<int> s;
-    int left=0, right=0;
-
     cin >> str;
-    
-    for(int i=0;str[i]!='\0';i++) {
-        if(str[i] == '(') left++;
-        else if(str[i] == ')') right++;
-    }
+    stack<int> s;
 
-    if(left==right) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    // 해결방법: 만약 여는 괄호가 온다면 그것을 스택에 넣고, 닫는 괄호가 온다면 스택에 있는 여는 괄호를 빼면 됩니다.
+    
+    for(int i=0;i<str.length();i++) {
+        if(str[i]=='(') {
+            s.push(1);
+        }
+        if(str[i]==')') {
+            if(s.size()<=0) {
+                cout << "No" << endl;
+                return 0;
+            }
+            else s.pop();
+        }
+    }
+    if(s.empty() == 0) {            // s.empty는 비어있을 때 1. 
+        cout << "No" << endl;
+    }
+    else cout << "Yes" << endl;
+    
 
     return 0;
 }
